116f5941c7c5ce473e93ff33e6c5b1ffffb578c3
Zarek Siegel <zsiegel@dh13.ucsd.edu>
2020-03-06 15:05:40 -0800

    added test scpt; began setup, ignores [2020-03-06 15:05:40 PST -0800]

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+	name="zauxpy",
+	description="zarek's auxiliary functions for python",
+	url="",
+	author="Zarek Siegel",
+	author_email="zarek.siegel@gmail.com",
+	license="",
+	packages=["formatting", "intan"],
+	zip_safe=False
+)
---
+++ zauxpy_testing_20200306.py
@@ -0,0 +1,8 @@
+#!/home/zsiegel/anaconda3/bin/python
+
+def main:
+	print(...)
+
+
+if __name__ == "__main__":
+	main()
